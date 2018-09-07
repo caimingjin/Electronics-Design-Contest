5b9deea76d8512949395fb70601e21c3f8ce3797
ame <1106682351@qq.com>
2018-09-07 20:37:02 +0800

    add project

---
+++ OBJ/common.d
@@ -0,0 +1,8 @@
+..\obj\common.o: ..\Gizwits\common.c
+..\obj\common.o: ..\Gizwits\common.h
+..\obj\common.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\common.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+..\obj\common.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+..\obj\common.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+..\obj\common.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\ctype.h
+..\obj\common.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
---
+++ OBJ/gizwits_protocol.d
@@ -0,0 +1,12 @@
+..\obj\gizwits_protocol.o: ..\Gizwits\gizwits_protocol.c
+..\obj\gizwits_protocol.o: ..\Gizwits\ringBuffer.h
+..\obj\gizwits_protocol.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\gizwits_protocol.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+..\obj\gizwits_protocol.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+..\obj\gizwits_protocol.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+..\obj\gizwits_protocol.o: ..\Gizwits\gizwits_product.h
+..\obj\gizwits_protocol.o: ..\Gizwits\gizwits_protocol.h
+..\obj\gizwits_protocol.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+..\obj\gizwits_protocol.o: ..\Gizwits\common.h
+..\obj\gizwits_protocol.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\ctype.h
+..\obj\gizwits_protocol.o: ..\Gizwits\dataPointTools.h
